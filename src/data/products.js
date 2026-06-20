4a01a9d072bbd6ca97f10a461834b02ea823bc75
gilsonfiho <gilson405719@hotmail.com>
2026-06-20 15:53:32 -0300

    feat: substitui Buquê Tiana por Buquê Encanto Rosê

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,6 @@
 import lirios from '../../assets/Buquê dois lírios (cor por sua escolha).jpeg';
 import rapunzel from '../../assets/Buquê Rapunzel com Led + Pascal.jpeg';
-import tiana from '../../assets/Buquê Tiana com Led.jpeg';
+import encantoRose from '../../assets/Buquê Encanto Rosê.jpeg';
 import merida from '../../assets/Buquê Princesa Merida.jpeg';
 import chaveiro from '../../assets/Chaveiro Pascal.png';
 import vanGogh from '../../assets/Buquê Van Gogh.png';
@@ -8,7 +8,7 @@ import vanGogh from '../../assets/Buquê Van Gogh.png';
 export const products = [
   { name: 'Buquê dois lírios (cor por sua escolha)', image: lirios },
   { name: 'Buquê Rapunzel com Led + Pascal', image: rapunzel },
-  { name: 'Buquê Tiana com Led', image: tiana },
+  { name: 'Buquê Encanto Rosê', image: encantoRose },
   { name: 'Buquê Princesa Merida', image: merida },
   { name: 'Buquê Van Gogh', image: vanGogh },
   { name: 'Chaveiro Pascal', image: chaveiro },
