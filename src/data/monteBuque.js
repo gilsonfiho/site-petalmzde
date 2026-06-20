54d56610c44a25dc9e21868c3cae4fa5d000dad5
gilsonfiho <gilson405719@hotmail.com>
2026-06-20 15:41:45 -0300

    feat: cria catálogo de flores e adicionais do Monte seu Buquê

---
+++ src/data/monteBuque.js
@@ -0,0 +1,37 @@
+import folhagem1 from '../../assets/MonteBuque/folhagem_(modelo_1).jpeg';
+import girassol1 from '../../assets/MonteBuque/girassol_(modelo1).jpeg';
+import lirio1 from '../../assets/MonteBuque/lirio_(modelo_1).jpeg';
+import lirio2 from '../../assets/MonteBuque/lirio_(modelo_2).jpeg';
+import lirio3 from '../../assets/MonteBuque/lirio_(modelo_3).jpeg';
+import lirioP from '../../assets/MonteBuque/lirio_(tamanho_p).jpeg';
+import lirioRapunzel from '../../assets/MonteBuque/lirio_rapunzel.jpeg';
+import margaridaP from '../../assets/MonteBuque/margarida_(tamanho_p).jpeg';
+import miniFlorzinhas from '../../assets/MonteBuque/mini_florzinhas_(ornamentação).jpeg';
+import rosa1 from '../../assets/MonteBuque/rosa_(modelo_1).jpeg';
+import tulipa from '../../assets/MonteBuque/tulipa.jpeg';
+
+import cartao from '../../assets/MonteBuque/Adicionais/cartao_mensagem.jpeg';
+import imagemAdicional from '../../assets/MonteBuque/Adicionais/imagem_adicional.jpeg';
+import led from '../../assets/MonteBuque/Adicionais/led.jpeg';
+import polaroid from '../../assets/MonteBuque/Adicionais/polaroid.jpeg';
+
+export const flores = [
+  { id: 'folhagem1',      name: 'Folhagem (Modelo 1)',          image: folhagem1 },
+  { id: 'girassol1',      name: 'Girassol (Modelo 1)',          image: girassol1 },
+  { id: 'lirio1',         name: 'Lírio (Modelo 1)',             image: lirio1 },
+  { id: 'lirio2',         name: 'Lírio (Modelo 2)',             image: lirio2 },
+  { id: 'lirio3',         name: 'Lírio (Modelo 3)',             image: lirio3 },
+  { id: 'lirioP',         name: 'Lírio (Tamanho P)',            image: lirioP },
+  { id: 'lirioRapunzel',  name: 'Lírio Rapunzel',              image: lirioRapunzel },
+  { id: 'margaridaP',     name: 'Margarida (Tamanho P)',        image: margaridaP },
+  { id: 'miniFlorzinhas', name: 'Mini Florzinhas (Ornamentação)', image: miniFlorzinhas },
+  { id: 'rosa1',          name: 'Rosa (Modelo 1)',              image: rosa1 },
+  { id: 'tulipa',         name: 'Tulipa',                      image: tulipa },
+];
+
+export const adicionais = [
+  { id: 'cartao',          name: 'Cartão Mensagem',  image: cartao },
+  { id: 'imagemAdicional', name: 'Imagem Adicional', image: imagemAdicional },
+  { id: 'led',             name: 'Led',              image: led },
+  { id: 'polaroid',        name: 'Polaroid',         image: polaroid },
+];
